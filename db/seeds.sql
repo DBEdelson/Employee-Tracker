cf409a9ebd2011e013d97b9bc3de2925fe4d2ab1
DBEdelson <DBEdelson8@gmail.com>
2022-10-26 19:41:07 -0500

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department (name)
+VALUES
+    ("Customer Service"),
+    ("Finance"),
+    ("Public Relations"),
+    ("Technology");
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ("Director", 80000, 3),
+    ("Department Head", 60000, 4),
+    ("Supervisor", 100000, 1),
+    ("Operator", 15000, 2);
+
+INSERT INTO employee(first_name, last_name)
+VALUES
+    ("Bill", "Smith"),
+    ("Bethany", "Rogers"),
+    ("Jason", "Falcon"),
+    ("Daniel", "Allen"),
+    ("Sarah", "Williamson");  
